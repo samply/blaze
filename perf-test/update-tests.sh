2bafed9f733d8b66a6314bc8cb53f3dc35ca83a0
Alexander Kiel <alexanderkiel@gmx.net>
2019-06-04 18:36:28 +0200

    Improve Performance of Generating Performance Test Cases

--- perf-test/update-tests.sh
+++ perf-test/update-tests.sh
@@ -1,45 +1,36 @@
 #!/usr/bin/env bash
 
 RATE=600
-ID_START=45000
-let "ID_END = $ID_START + $RATE * 30"
+ID_START=0
 
 
 echo "Patient Update Tests:"
-for (( ID=$ID_START; ID<$ID_END; ID++))
-do
-  cat patient-update.json | \
-  jq -cM --arg id ${ID} '.url += $id | .body.id = $id | .body |= @base64'
-done | \
+cat patient-update.json | \
+jq -cM --argjson start ${ID_START} --argjson rate ${RATE} \
+  '. as $request | range($start; $start + $rate * 30) | tostring as $id | $request | .url += $id | .body.id = $id | .body |= @base64' | \
 vegeta attack -rate=${RATE} -format=json -duration=30s | \
 vegeta report
 
 
 echo "Condition Update Tests:"
-for (( ID=$ID_START; ID<$ID_END; ID++))
-do
-  cat condition-update.json | \
-  jq -cM --arg id ${ID} '.url += $id | .body.id = $id | .body.subject.reference += $id | .body |= @base64'
-done | \
+cat condition-update.json | \
+jq -cM --argjson start ${ID_START} --argjson rate ${RATE} \
+  '. as $request | range($start; $start + $rate * 30) | tostring as $id | $request | .body.id = $id | .url += $id | .body.subject.reference += $id | .body |= @base64' | \
 vegeta attack -rate=${RATE} -format=json -duration=30s | \
 vegeta report
 
 
 echo "Observation Update Tests:"
-for (( ID=$ID_START; ID<$ID_END; ID++))
-do
-  cat observation-update.json | \
-  jq -cM --arg id ${ID} '.url += $id | .body.id = $id | .body.subject.reference += $id | .body |= @base64'
-done | \
+cat observation-update.json | \
+jq -cM --argjson start ${ID_START} --argjson rate ${RATE} \
+  '. as $request | range($start; $start + $rate * 30) | tostring as $id | $request | .body.id = $id | .url += $id | .body.subject.reference += $id | .body |= @base64' | \
 vegeta attack -rate=${RATE} -format=json -duration=30s | \
 vegeta report
 
 
 echo "Specimen Update Tests:"
-for (( ID=$ID_START; ID<$ID_END; ID++))
-do
-  cat specimen-update.json | \
-  jq -cM --arg id ${ID} '.url += $id | .body.id = $id | .body.subject.reference += $id | .body |= @base64'
-done | \
+cat specimen-update.json | \
+jq -cM --argjson start ${ID_START} --argjson rate ${RATE} \
+  '. as $request | range($start; $start + $rate * 30) | tostring as $id | $request | .body.id = $id | .url += $id | .body.subject.reference += $id | .body |= @base64' | \
 vegeta attack -rate=${RATE} -format=json -duration=30s | \
 vegeta report
