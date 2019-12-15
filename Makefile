b4806aaca9c94b5296fb52da530c5379ba75f565
Alexander Kiel <alexanderkiel@gmx.net>
2019-12-15 21:30:30 +0100

    Version 0.8.0-alpha.3

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = "0.8.0-alpha.2"
+VERSION = "0.8.0-alpha.3"
 
 check:
 	clojure -A:check
--- src/blaze/system.clj
+++ src/blaze/system.clj
@@ -77,7 +77,7 @@
 
 
 (def ^:private root-config
-  {:blaze/version "0.8.0-alpha.2"
+  {:blaze/version "0.8.0-alpha.3"
 
    :blaze/clock {}
 
