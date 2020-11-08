97552555902ce1c2a413d67bc3d231b4e105e238
Alexander Kiel <alexanderkiel@gmx.net>
2020-11-08 15:11:38 +0100

    Release v0.10.0.alpha.3

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION := "0.10.0-alpha.2"
+VERSION := "0.10.0-alpha.3"
 MODULES := $(wildcard modules/*)
 
 $(MODULES):
--- src/blaze/system.clj
+++ src/blaze/system.clj
@@ -77,7 +77,7 @@
 
 
 (def ^:private root-config
-  {:blaze/version "0.10.0-alpha.2"
+  {:blaze/version "0.10.0-alpha.3"
 
    :blaze/structure-definition {}
 
