626e9c2a0ccb96d7ea184917a7280e75b12111b0
Alexander Kiel <alexanderkiel@gmx.net>
2020-12-15 15:55:44 +0100

    Fix Failing Tests

--- modules/db/test/blaze/db/node/validation_test.clj
+++ modules/db/test/blaze/db/node/validation_test.clj
@@ -3,7 +3,12 @@
     [blaze.db.node.validation :as validation]
     [blaze.db.node.validation-spec]
     [clojure.spec.test.alpha :as st]
-    [clojure.test :as test :refer [deftest is]]))
+    [clojure.test :as test :refer [deftest is testing]]
+    [cognitect.anomalies :as anom]
+    [juxt.iota :refer [given]]))
+
+
+(st/instrument)
 
 
 (defn fixture [f]
@@ -16,4 +21,12 @@
 
 
 (deftest validate-ops
-  (is (nil? (validation/validate-ops []))))
+  (testing "single delete"
+    (is (nil? (validation/validate-ops [[:delete "Patient" "0"]]))))
+
+  (testing "duplicate delete"
+    (given (validation/validate-ops [[:delete "Patient" "0"]
+                                     [:delete "Patient" "0"]])
+      ::anom/category := ::anom/incorrect
+      :cognitect.anomalies/message := "Duplicate resource `Patient/0`.",
+      :fhir/issue := "invariant")))
