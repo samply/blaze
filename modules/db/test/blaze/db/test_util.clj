72715416e44f088084e2dbcc1345f2e12ae58ed6
Alexander Kiel <alexanderkiel@gmx.net>
2021-04-22 17:41:46 +0200

    Improve DB Tests

--- modules/db/test/blaze/db/test_util.clj
+++ modules/db/test/blaze/db/test_util.clj
@@ -1,8 +1,9 @@
 (ns blaze.db.test-util
   (:require
+    [clojure.test :as test :refer [is]]
     [juxt.iota :refer [given]]))
 
 
 (defmacro given-thrown [v & body]
-  `(given (try ~v (catch Exception e# (ex-data e#)))
+  `(given (try ~v (is false) (catch Exception e# (ex-data e#)))
      ~@body))
