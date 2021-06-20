fc924958c9592c9880a3ead14df318a1584f9f04
Alexander Kiel <alexanderkiel@gmx.net>
2021-06-20 19:13:33 +0200

    Cleanup

--- .github/scripts/revinclude.sh
+++ .github/scripts/revinclude.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S bash -e
+#!/bin/bash -e
 
 BASE=http://localhost:8080/fhir
 EXPECTED_NUM_PATIENTS=$(curl -s "${BASE}/Patient?_summary=count" | jq -r .total)
@@ -7,29 +7,29 @@ EXPECTED_NUM_CONDITIONS=$(curl -s "${BASE}/Condition?_summary=count" | jq -r .to
 EXPECTED_NUM_ENCOUNTERS=$(curl -s "${BASE}/Encounter?_summary=count" | jq -r .total)
 EXPECTED_NUM_PROCEDURES=$(curl -s "${BASE}/Procedure?_summary=count" | jq -r .total)
 
-./blazectl --server $BASE download -t Patient -q '_revinclude=Observation:subject&_revinclude=Condition:subject&_revinclude=Procedure:subject&_revinclude=Encounter:subject' -o output.ndjson
+blazectl --server $BASE download -t Patient -q '_revinclude=Observation:subject&_revinclude=Condition:subject&_revinclude=Procedure:subject&_revinclude=Encounter:subject' -o output.ndjson
 
-ACTUAL_NUM_PATIENTS=$(cat output.ndjson | jq -r .resourceType | grep Patient | wc -l | xargs | cut -d ' ' -f1)
-ACTUAL_NUM_OBSERVATIONS=$(cat output.ndjson | jq -r .resourceType | grep Observation | wc -l | xargs | cut -d ' ' -f1)
-ACTUAL_NUM_CONDITIONS=$(cat output.ndjson | jq -r .resourceType | grep Condition | wc -l | xargs | cut -d ' ' -f1)
-ACTUAL_NUM_ENCOUNTERS=$(cat output.ndjson | jq -r .resourceType | grep Encounter | wc -l | xargs | cut -d ' ' -f1)
-ACTUAL_NUM_PROCEDURES=$(cat output.ndjson | jq -r .resourceType | grep Procedure | wc -l | xargs | cut -d ' ' -f1)
+ACTUAL_NUM_PATIENTS=$(jq -r .resourceType output.ndjson | grep -c Patient)
+ACTUAL_NUM_OBSERVATIONS=$(jq -r .resourceType output.ndjson | grep -c Observation)
+ACTUAL_NUM_CONDITIONS=$(jq -r .resourceType output.ndjson | grep -c Condition)
+ACTUAL_NUM_ENCOUNTERS=$(jq -r .resourceType output.ndjson | grep -c Encounter)
+ACTUAL_NUM_PROCEDURES=$(jq -r .resourceType output.ndjson | grep -c Procedure)
 
 rm output.ndjson
 
-if [ $EXPECTED_NUM_PATIENTS != $ACTUAL_NUM_PATIENTS ]; then
+if [ "$EXPECTED_NUM_PATIENTS" != "$ACTUAL_NUM_PATIENTS" ]; then
   echo "Fail: Patient download size was ${ACTUAL_NUM_PATIENTS} but should be ${EXPECTED_NUM_PATIENTS}"
   exit 1
-elif [ $EXPECTED_NUM_OBSERVATIONS != $ACTUAL_NUM_OBSERVATIONS ]; then
+elif [ "$EXPECTED_NUM_OBSERVATIONS" != "$ACTUAL_NUM_OBSERVATIONS" ]; then
   echo "Fail: Observation download size was ${ACTUAL_NUM_OBSERVATIONS} but should be ${EXPECTED_NUM_OBSERVATIONS}"
   exit 1
-elif [ $EXPECTED_NUM_CONDITIONS != $ACTUAL_NUM_CONDITIONS ]; then
+elif [ "$EXPECTED_NUM_CONDITIONS" != "$ACTUAL_NUM_CONDITIONS" ]; then
   echo "Fail: Condition download size was ${ACTUAL_NUM_CONDITIONS} but should be ${EXPECTED_NUM_CONDITIONS}"
   exit 1
-elif [ $EXPECTED_NUM_ENCOUNTERS != $ACTUAL_NUM_ENCOUNTERS ]; then
+elif [ "$EXPECTED_NUM_ENCOUNTERS" != "$ACTUAL_NUM_ENCOUNTERS" ]; then
   echo "Fail: Encounter download size was ${ACTUAL_NUM_ENCOUNTERS} but should be ${EXPECTED_NUM_ENCOUNTERS}"
   exit 1
-elif [ $EXPECTED_NUM_PROCEDURES != $ACTUAL_NUM_PROCEDURES ]; then
+elif [ "$EXPECTED_NUM_PROCEDURES" != "$ACTUAL_NUM_PROCEDURES" ]; then
   echo "Fail: Procedure download size was ${ACTUAL_NUM_PROCEDURES} but should be ${EXPECTED_NUM_PROCEDURES}"
   exit 1
 else
--- .github/scripts/x-forwarded-headers.sh
+++ .github/scripts/x-forwarded-headers.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env -S bash -e
+#!/bin/bash -e
 
 SELF_LINK=$(curl -s -H 'X-Forwarded-Host:blaze.de' -H 'X-Forwarded-Proto:https' http://localhost:8080/fhir/Patient | jq -r .link[0].url | cut -d? -f1)
 
-if [ "https://blaze.de/fhir/Patient" = $SELF_LINK ]; then
+if [ "https://blaze.de/fhir/Patient" = "$SELF_LINK" ]; then
   echo "Success"
   exit 0
 else
