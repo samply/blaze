316cb74af06ef9f498aa4ef50cd383bea2aaf2d2
Alexander Kiel <alexanderkiel@gmx.net>
2021-07-10 12:23:13 +0200

    Use Stable Test Data

--- .github/scripts/search-observation-profile.sh
+++ .github/scripts/search-observation-profile.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
 
 BASE=http://localhost:8080/fhir
-TOTAL_COUNT=$(curl -s "${BASE}/Observation?_summary=count" | jq -r .total)
 LAB_COUNT=$(curl -s "${BASE}/Observation?_profile=http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-lab&_summary=count" | jq -r .total)
 
-if [ $LAB_COUNT -lt $TOTAL_COUNT ]; then
-  echo "Success: lab count ($LAB_COUNT) < total count ($TOTAL_COUNT)"
+if [ "$LAB_COUNT" = "27218" ]; then
+  echo "Success: lab count ($LAB_COUNT) equals the expected count"
   exit 0
 else
-  echo "Fail: lab count ($LAB_COUNT) !< total count ($TOTAL_COUNT)"
+  echo "Fail: lab count ($LAB_COUNT) != 27218"
   exit 1
 fi
