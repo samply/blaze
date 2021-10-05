f0ae3fb2755add00e5ae570a39e2d4568bde06d7
Alexander Kiel <alexanderkiel@gmx.net>
2021-10-05 15:51:58 +0200

    Fix Failing OPTIONS Request
    Closes: #466

--- dev/blaze/dev/db.clj
+++ dev/blaze/dev/db.clj
@@ -5,29 +5,29 @@
     [com.github.benmanes.caffeine.cache Cache]))
 
 
-(def ^Cache resource-cache
+(defn ^Cache resource-cache []
   (.synchronous (.-cache (:blaze.db/resource-cache system))))
 
 
-(def ^Cache resource-handle-cache
+(defn ^Cache resource-handle-cache []
   (:blaze.db/resource-handle-cache system))
 
 
-(def ^Cache tx-cache
+(defn ^Cache tx-cache []
   (:blaze.db/tx-cache system))
 
 
-(def node
+(defn node []
   (:blaze.db/node system))
 
 
 (comment
-  (.estimatedSize resource-cache)
-  (.invalidateAll resource-cache)
+  (.estimatedSize (resource-cache))
+  (.invalidateAll (resource-cache))
 
-  (.estimatedSize resource-handle-cache)
-  (.invalidateAll resource-handle-cache)
+  (.estimatedSize (resource-handle-cache))
+  (.invalidateAll (resource-handle-cache))
 
-  (.estimatedSize tx-cache)
-  (.invalidateAll tx-cache)
+  (.estimatedSize (tx-cache))
+  (.invalidateAll (tx-cache))
   )
