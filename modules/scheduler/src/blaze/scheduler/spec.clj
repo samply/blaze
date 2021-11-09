d07a4eed00f0946175f4bb62c76e3ba88c5d1192
Alexander Kiel <alexanderkiel@gmx.net>
2021-11-09 20:28:17 +0100

    Improve Scheduler Coverage

--- modules/scheduler/src/blaze/scheduler/spec.clj
+++ modules/scheduler/src/blaze/scheduler/spec.clj
@@ -1,8 +1,14 @@
 (ns blaze.scheduler.spec
   (:require
     [blaze.scheduler.protocol :as p]
-    [clojure.spec.alpha :as s]))
+    [clojure.spec.alpha :as s])
+  (:import
+    [java.util.concurrent ScheduledFuture]))
 
 
 (s/def :blaze/scheduler
   #(satisfies? p/Scheduler %))
+
+
+(s/def :blaze.scheduler/future
+  #(instance? ScheduledFuture %))
--- modules/scheduler/src/blaze/scheduler_spec.clj
+++ modules/scheduler/src/blaze/scheduler_spec.clj
@@ -3,12 +3,15 @@
     [blaze.scheduler :as sched]
     [blaze.scheduler.spec]
     [clojure.spec.alpha :as s]
-    [java-time :as time])
-  (:import
-    [java.util.concurrent ScheduledFuture]))
+    [java-time :as time]))
 
 
 (s/fdef sched/schedule-at-fixed-rate
   :args (s/cat :scheduler :blaze/scheduler :f fn?
                :initial-delay time/duration? :period time/duration?)
-  :ret #(instance? ScheduledFuture %))
+  :ret :blaze.scheduler/future)
+
+
+(s/fdef sched/cancel
+  :args (s/cat :future :blaze.scheduler/future :interrupt-if-running? boolean?)
+  :ret boolean?)
