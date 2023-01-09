ff7961040dd83df113dfa0b843e74d7366ad1b66
Alexander Kiel <alexanderkiel@gmx.net>
2023-01-09 18:11:28 +0100

    Add Measure ID to Anomaly

--- modules/interaction/test/blaze/interaction/transaction/bundle/url_spec.clj
+++ modules/interaction/test/blaze/interaction/transaction/bundle/url_spec.clj
@@ -8,5 +8,5 @@
 (s/fdef url/match-url
   :args (s/cat :url string?)
   :ret (s/or :type-level (s/tuple :fhir.resource/type)
-             :instance-level (s/tuple :fhir.resource/type :blaze.resource/id)
+             :instance-level :blaze.fhir/local-ref-tuple
              :other nil?))
