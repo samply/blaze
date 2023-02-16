b38743de67110962dc5b792d79ebbad8c48e16b3
Alexander Kiel <alexanderkiel@gmx.net>
2023-02-16 17:37:37 +0100

    Update Blazectl

--- .github/scripts/download-all-resources.sh
+++ .github/scripts/download-all-resources.sh
@@ -7,6 +7,6 @@ BASE="http://localhost:8080/fhir"
 EXPECTED_SIZE=$(curl -s "$BASE?_summary=count" | jq -r .total)
 
 FILE_NAME=$(uuidgen)
-blazectl --no-progress --server $BASE download "" -o "$FILE_NAME".ndjson
+blazectl --no-progress --server "$BASE" download -o "$FILE_NAME.ndjson"
 
-test "download size" "$(wc -l "$FILE_NAME".ndjson | xargs | cut -d ' ' -f1)" "$EXPECTED_SIZE"
+test "download size" "$(wc -l "$FILE_NAME.ndjson" | xargs | cut -d ' ' -f1)" "$EXPECTED_SIZE"
