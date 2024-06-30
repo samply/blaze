923647afd834cfaa28927fdfb1cb3ca44f6e844e
Alexander Kiel <alexanderkiel@gmx.net>
2024-06-30 17:38:23 +0200

    Suppress Log Output at Code Coverage Runs
    While running normal unit tests, the log output is captured by Kaocha.
    However for code coverage runs via Cloverage that was not the case.
    Having such enormous log output is bad for GitHub actions and
    potentially slower. Using the Cloverage plugin of Kaocha, we are now
    able to capture the log output also for coverage runs.

--- modules/luid/test-perf/blaze/luid_test_perf.clj
+++ modules/luid/test-perf/blaze/luid_test_perf.clj
@@ -1,13 +1,10 @@
 (ns blaze.luid-test-perf
   (:require
-    [blaze.luid :as luid]
-    [clojure.test :refer [deftest is testing]]
-    [criterium.core :as criterium]))
-
+   [blaze.luid :as luid]
+   [clojure.test :refer [deftest is testing]]
+   [criterium.core :as criterium]))
 
 (comment
   (let [clock (Clock/systemUTC)
         rng (ThreadLocalRandom/current)]
-    (criterium/quick-bench (luid/luid clock rng)))
-
-  )
+    (criterium/quick-bench (luid/luid clock rng))))
