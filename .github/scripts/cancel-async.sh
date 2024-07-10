4868173fdeb5eac7dc0a5d87632cd51a859e30b9
Alexander Kiel <alexanderkiel@gmx.net>
2024-07-10 19:19:05 +0200

    Ensure that Async Status Endpoint uses OperationOutcome
    Closes: 1886

--- .github/scripts/cancel-async.sh
+++ .github/scripts/cancel-async.sh
@@ -11,6 +11,9 @@ STATUS_CODE=$(curl -s -XDELETE -o /dev/null -w '%{response_code}' "$STATUS_URL")
 test "cancel status code" "$STATUS_CODE" "202"
 
 test "status code after cancel" "$(curl -s -o /dev/null -w '%{response_code}' "$STATUS_URL")" "404"
+RESPONSE="$(curl -s "$STATUS_URL")"
+test "after cancel resource type" "$(echo "$RESPONSE" | jq -r .resourceType)" "OperationOutcome"
+test "after cancel issue code" "$(echo "$RESPONSE" | jq -r '.issue[0].code')" "not-found"
 
 DIAGNOSTICS="$(curl -s -H 'Accept: application/fhir+json' "$STATUS_URL" | jq -r '.issue[0].diagnostics')"
 if [[ "$DIAGNOSTICS" =~ The\ asynchronous\ request\ with\ id\ \`[A-Z0-9]+\`\ is\ cancelled. ]]; then
