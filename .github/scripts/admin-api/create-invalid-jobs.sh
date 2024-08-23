693380b8fb1a227da0a4261d9aba4f3f86b2444d
Alexander Kiel <alexanderkiel@gmx.net>
2024-08-23 13:07:43 +0200

    Update HAPI to v7.4.0

--- .github/scripts/admin-api/create-invalid-jobs.sh
+++ .github/scripts/admin-api/create-invalid-jobs.sh
@@ -17,10 +17,35 @@ cat <<END
       "https://samply.github.io/blaze/fhir/StructureDefinition/ReIndexJob"
     ]
   },
-  "status": "on-hold"
+  "status": "on-hold",
+  "intent": "order",
+  "code": {
+    "coding": [
+      {
+        "code": "re-index",
+        "system": "https://samply.github.io/blaze/fhir/CodeSystem/JobType",
+        "display": "(Re)Index a Search Parameter"
+      }
+    ]
+  },
+  "authoredOn": "2024-04-13T10:05:20.927Z",
+  "input": [
+    {
+      "type": {
+        "coding": [
+          {
+            "code": "search-param-url",
+            "system": "https://samply.github.io/blaze/fhir/CodeSystem/ReIndexJobParameter",
+            "display": "Search Param URL"
+          }
+        ]
+      },
+      "valueCanonical": "http://hl7.org/fhir/SearchParameter/Resource-profile"
+    }
+  ]
 }
 END
 }
 
 ERROR_MESSAGE="$(curl -s -H 'Content-Type: application/fhir+json' -H 'Accept: application/fhir+json' -d "$(re-index-job)" "$BASE/__admin/Task" | jq -r '.issue[0].diagnostics')"
-test "error message" "$ERROR_MESSAGE" "Rule status-reason-on-hold: 'Assigns possible reasons to the 'on-hold' status.' Failed"
+test "error message" "$ERROR_MESSAGE" "Constraint failed: status-reason-on-hold: 'Assigns possible reasons to the 'on-hold' status.' (defined in https://samply.github.io/blaze/fhir/StructureDefinition/Job)"
