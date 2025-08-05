bab2286c199d164160fe1e30c6aafbaf34e158ec
Alexander Kiel <alexanderkiel@gmx.net>
2025-08-05 21:09:10 +0200

    Update blazectl

--- .github/scripts/install-blazectl.sh
+++ .github/scripts/install-blazectl.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -e
 
-VERSION="0.16.0"
+curl -sSfL https://raw.githubusercontent.com/samply/blazectl/main/install.sh |
+  sh -s "$BLAZECTL_VERSION"
 
-curl -sLO "https://github.com/samply/blazectl/releases/download/v$VERSION/blazectl-$VERSION-linux-amd64.tar.gz"
-tar xzf "blazectl-$VERSION-linux-amd64.tar.gz"
-rm "blazectl-$VERSION-linux-amd64.tar.gz"
 sudo mv ./blazectl /usr/local/bin/blazectl
