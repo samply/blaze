ca042e6a5ab6e0a7f15b0b32362cdca7e8e30936
Jonas Wagner <jwagner@knoppiks.de>
2025-08-25 10:20:19 +0200

    Pin Blazectl Installation
    Signed-off-by: Jonas Wagner <jwagner@knoppiks.de>

--- .github/scripts/install-blazectl.sh
+++ .github/scripts/install-blazectl.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
-curl -sSfL https://raw.githubusercontent.com/samply/blazectl/main/install.sh |
-  sh -s "$BLAZECTL_VERSION"
+url="https://github.com/samply/blazectl/releases/download/v${BLAZECTL_VERSION}/blazectl-${BLAZECTL_VERSION}-linux-amd64.tar.gz"
+curl -sSfL "${url}" >blazectl.tar.gz
+echo "${BLAZECTL_CHECKSUM} blazectl.tar.gz" | sha256sum -c
 
-sudo mv ./blazectl /usr/local/bin/blazectl
+tar -xzf blazectl.tar.gz
+mkdir -p ~/.local/bin/
+mv blazectl ~/.local/bin/blazectl
