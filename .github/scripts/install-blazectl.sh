baa6f1a37803137a9945fe38fc21a79792662489
Jonas Wagner <jwagner@knoppiks.de>
2025-10-13 15:06:59 +0200

    Fix Blazectl Version Updates
    Signed-off-by: Jonas Wagner <jwagner@knoppiks.de>

--- .github/scripts/install-blazectl.sh
+++ .github/scripts/install-blazectl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-url="https://github.com/samply/blazectl/releases/download/v${BLAZECTL_VERSION}/blazectl-${BLAZECTL_VERSION}-linux-amd64.tar.gz"
+url="https://github.com/samply/blazectl/releases/download/${BLAZECTL_VERSION}/blazectl-${BLAZECTL_VERSION#v}-linux-amd64.tar.gz"
 curl -sSfL "${url}" >blazectl.tar.gz
 echo "${BLAZECTL_CHECKSUM} blazectl.tar.gz" | sha256sum -c
 
