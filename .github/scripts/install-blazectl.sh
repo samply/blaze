663c71c4c47771626f93227f7bf171cccb730153
Alexander Kiel <alexanderkiel@gmx.net>
2025-11-07 15:54:55 +0100

    Renovate Improvements
    * add docker datasource to weekly updates
    * enable FHIR Validator updates

--- .github/scripts/install-blazectl.sh
+++ .github/scripts/install-blazectl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-url="https://github.com/samply/blazectl/releases/download/${BLAZECTL_VERSION}/blazectl-${BLAZECTL_VERSION#v}-linux-amd64.tar.gz"
-curl -sSfL "${url}" >blazectl.tar.gz
-echo "${BLAZECTL_CHECKSUM} blazectl.tar.gz" | sha256sum -c
+URL="https://github.com/samply/blazectl/releases/download/$BLAZECTL_VERSION/blazectl-${BLAZECTL_VERSION#v}-linux-amd64.tar.gz"
+curl -sSfL "$URL" > blazectl.tar.gz
+echo "$BLAZECTL_CHECKSUM blazectl.tar.gz" | sha256sum -c
 
 tar -xzf blazectl.tar.gz
 mkdir -p ~/.local/bin/
