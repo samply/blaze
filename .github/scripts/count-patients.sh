0856331d9f7f39315cc38ebae65ecdf269d0a170
Alexander Kiel <alexanderkiel@gmx.net>
2025-12-11 11:46:34 +0100

    Fix Flaky Query Plan Test

--- .github/scripts/count-patients.sh
+++ .github/scripts/count-patients.sh
@@ -7,6 +7,10 @@ base="http://localhost:8080/fhir"
 type=$1
 query="${2//[[:space:]]/}"
 expected_size=$3
-actual_size=$(blazectl --server "$base" download "$type" -q "$query" 2>/dev/null | jq -r .subject.reference | sort -u | wc -l | xargs)
+
+page_size="$(shuf -i 50-500 -n 1)"
+echo "ℹ️ use a page size of $page_size"
+
+actual_size=$(blazectl --server "$base" download "$type" -q "$query&_count=$page_size" 2>/dev/null | jq -r .subject.reference | sort -u | wc -l | xargs)
 
 test "number of patients" "$actual_size" "$expected_size"
