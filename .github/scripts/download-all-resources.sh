fac97041a34e7498dd1c9af3e971025fdbb17c51
Alexander Kiel <alexanderkiel@gmx.net>
2026-03-23 16:36:10 +0100

    Optimize Distributed Test Performance

--- .github/scripts/download-all-resources.sh
+++ .github/scripts/download-all-resources.sh
@@ -6,7 +6,7 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 base="http://localhost:8080/fhir"
 expected_size_json=$(curl -sH 'Accept: application/fhir+json' "$base?_summary=count" | jq -r .total)
 expected_size_xml=$(curl -sH 'Accept: application/fhir+xml' "$base?_summary=count" | xq -x /Bundle/total/@value)
-actual_size=$(blazectl --server "$base" -q '_count=1000' download 2>/dev/null | wc -l | xargs)
+actual_size=$(blazectl --server "$base" -q '_count=1000' download | wc -l | xargs)
 
 test "totals (JSON, XML) match" "$expected_size_json" "$expected_size_xml"
 test "download size" "$actual_size" "$expected_size_json"
