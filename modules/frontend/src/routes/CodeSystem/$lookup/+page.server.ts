e626d59d8a8c847b43bf2271e7791ab44f632dee
Tobias Machein <tobias.machein@kf-interactive.com>
2026-04-01 10:57:19 +0200

    Add CodeSystem $lookup operation to Frontend UI
    #3521

---
+++ modules/frontend/src/routes/CodeSystem/$lookup/+page.server.ts
@@ -0,0 +1,78 @@
+import type { Actions } from './$types';
+import type { OperationOutcome, Parameters, ParametersParameter } from 'fhir/r4';
+import { resolve } from '$app/paths';
+import { fail } from '@sveltejs/kit';
+
+export const actions = {
+  default: async ({ request, fetch }) => {
+    const data = await request.formData();
+    const system = data.get('system') as string;
+    const version = data.get('version') as string;
+    const code = data.get('code') as string;
+    const display = data.get('display') as string;
+    const displayLanguage = data.get('displayLanguage') as string;
+
+    const parameters: ParametersParameter[] = [
+      {
+        name: 'system',
+        valueUri: system
+      },
+      {
+        name: 'code',
+        valueCode: code
+      }
+    ];
+
+    if (version !== '') {
+      parameters.push({
+        name: 'version',
+        valueString: version
+      });
+    }
+
+    if (display !== '') {
+      parameters.push({
+        name: 'display',
+        valueString: display
+      });
+    }
+
+    if (displayLanguage !== '') {
+      parameters.push({
+        name: 'displayLanguage',
+        valueCode: displayLanguage
+      });
+    }
+
+    const res = await fetch(resolve('/CodeSystem/$lookup'), {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/fhir+json', Accept: 'application/fhir+json' },
+      body: JSON.stringify({
+        resourceType: 'Parameters',
+        parameter: parameters
+      })
+    });
+
+    if (!res.ok) {
+      const error: OperationOutcome = await res.json();
+      return fail(400, {
+        system,
+        version,
+        code,
+        display,
+        displayLanguage,
+        incorrect: true,
+        msg: error.issue[0]?.diagnostics ?? error.issue[0]?.details?.text
+      });
+    }
+
+    return {
+      system,
+      version,
+      code,
+      display,
+      displayLanguage,
+      result: (await res.json()) as Parameters
+    };
+  }
+} satisfies Actions;
---
+++ modules/frontend/src/routes/CodeSystem/$lookup/result-list.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import type { Parameters } from 'fhir/r4';
+
+  import { parameter } from '$lib/fhir';
+  import DescriptionList from '$lib/tailwind/description/left-aligned/list.svelte';
+  import Row from '$lib/tailwind/description/left-aligned/row-3-2.svelte';
+
+  interface Props {
+    parameters: Parameters;
+  }
+
+  let { parameters }: Props = $props();
+
+    console.log(parameters);
+    console.log(parameter(parameters, 'version'));
+  let result = $derived(parameter(parameters, 'result')?.valueBoolean);
+</script>
+
+<DescriptionList>
+  <Row title="Name">
+    {parameter(parameters, 'name')?.valueString}
+  </Row>
+  {@const version = parameter(parameters, 'version')?.valueString}
+  {#if version}
+    <Row title="Version">
+      {version}
+    </Row>
+  {/if}
+  {@const display = parameter(parameters, 'display')?.valueString}
+  {#if display}
+    <Row title="Display">
+      {display}
+    </Row>
+  {/if}
+  {@const definition = parameter(parameters, 'definition')?.valueString}
+  {#if definition}
+    <Row title="Definition">
+      {definition}
+    </Row>
+  {/if}
+</DescriptionList>
