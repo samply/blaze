f0566509db4652c3f55553fd9dd311f80753ed0a
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2026-07-29 11:50:02 +0200

    Add Checksum Validation to Actionlint Install
    Closes: #3782
    Signed-off-by: Daniel Hahne <daniel.hahne@uni-leipzig.de>

--- .github/scripts/install-actionlint.sh
+++ .github/scripts/install-actionlint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-url="https://github.com/rhysd/actionlint/releases/download/v${ACTIONLINT_VERSION}/actionlint_${ACTIONLINT_VERSION}_linux_amd64.tar.gz"
+url="https://github.com/rhysd/actionlint/releases/download/$ACTIONLINT_VERSION/actionlint_${ACTIONLINT_VERSION#v}_linux_amd64.tar.gz"
 curl -sSfL "$url" > actionlint.tar.gz
+echo "$ACTIONLINT_CHECKSUM actionlint.tar.gz" | sha256sum -c
 
 tar -xzf actionlint.tar.gz
 mkdir -p ~/.local/bin/
