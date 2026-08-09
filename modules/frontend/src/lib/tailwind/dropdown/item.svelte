469435bd8a36452a077adaedb06f453bab454768
Alexander Kiel <alexanderkiel@gmx.net>
2026-08-09 10:09:26 +0000

    Fix Dropdown Item Hover Color in Dark Mode
    Closes: #4061
    Signed-off-by: Alexander Kiel <alexanderkiel@gmx.net>

--- modules/frontend/src/lib/tailwind/dropdown/item.svelte
+++ modules/frontend/src/lib/tailwind/dropdown/item.svelte
@@ -14,7 +14,7 @@
 <!-- Active: "bg-gray-100 text-gray-900 dark:text-gray-100 outline-none", Not Active: "text-gray-700 dark:text-gray-300" -->
 <a
   href={url}
-  class="hhover:dark:text-gray-100 block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-300 hover:dark:bg-gray-600"
+  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-300 hover:dark:bg-gray-600 hover:dark:text-gray-100"
   role="menuitem"
   tabindex="-1"
   id="menu-item-0">{name}</a
